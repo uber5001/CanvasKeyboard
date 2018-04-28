991bda19a8b8e49713c56b19dd8fff8e3c9e23cd
Scott Little <uber5001@gmail.com>
2018-04-28 17:41:52 -0600

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -25,8 +26,9 @@ module.exports = {
             filename: 'index.html',
             inject: 'head',
             cache: false,
-            meta: 'width=device-width, initial-scale=1'
+            meta: 'width=device-width, initial-scale=1',
+            inlineSource: '.(js|css)$'
         }),
-        // new HtmlWebpackInlineSourcePlugin()
+        new HtmlWebpackInlineSourcePlugin()
     ]
 }
